3dfe02d2f6b2439dca3859b8ce43f49fb2e40a22
mwoolf87 <michael.j.woolf@gmail.com>
2020-05-17 19:02:43 -0400

    installed axios and unsplashAPI

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: "10px" }}>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends React.Component {
+  onSearchSubmit(term) {
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID dgQ4Pj26x73pM-1xkAooREYCb5RfsoqqbEnWesB6fkM",
+      },
+    });
+  }
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
 export default App;
